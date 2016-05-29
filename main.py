a527c53796a5fa1e9d43de0823bde588fe7e9a41
slrc1 <info@slrc1.comli.com>
2016-05-29 21:45:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ for item in sys.argv:
   f.write("%s\n" % item)
 f.close()
 
-s = socket()
-s.connect('localhost',sys.argv[1])
+ss = socket()
+ss.bind('localhost',sys.argv[1])
+ss.listen()
+s,a = ss.accept()
 s.sendall(b'hello')
 s.close()
+ss.close()
